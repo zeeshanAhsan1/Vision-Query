4ed3dbc2d3921574d4f90823a9bdd98afb9c8f13
Zeeshan Ahsan <zeeshan.ahsan15@gmail.com>
2024-06-03 20:04:11 -0400

    Add code and API route to upload an image and ask

--- app/main.py
+++ app/main.py
@@ -12,4 +12,15 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+
+@app.post("/ask")
+def ask_query(text: str, image: UploadFile):
+    content = image.file.read()
+    
+    image = Image.open(io.BytesIO(content))
+    # image = Image.open(image.file)
+    
+    result = model_prediction(text, image)
+    return {"answer": result}
     
